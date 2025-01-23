aa762f550efc188305588b6f3ca2299c02e4adf9
Joshua Kuminski <joshuakuminski.nysdot@gmail.com>
2025-01-22 20:55:55 -0500

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 Mio_config = {
     "server":"localhost\SQLEXPRESS",
-    "database":"MIOVISION2",
-    "username":"jkuminski",
-    "password":"Radlich-1989",
-    "api_key":"7_J6IKTAlbukBQJdwON9x0G_wYq0oZyr9t3ksZy7BJH3ASEFwVjJC-I95p9Cus6i"
+    "database":"DATABASE",
+    "username":"username",
+    "password":"password",
+    "api_key":"api_key"
 }
